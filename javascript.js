8a7d651d9abeee76b7b0e293bf903f9d51ca69e7
Eric Todasco <erictodasco@me.com>
2024-09-18 15:59:39 -0400

    Profile Update

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-// Click Event Listener
+// Avatar click event Listener //
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -29,11 +29,86 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Button click event lister for each frontend button //
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll(".clickable").forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.currentTarget.classList.toggle("active"); // Toggling only the 'active' class
-      event.currentTarget.classList.toggle("i"); // Toggling the 'i' class separately if needed
+
+  const btn_git = document.querySelector(".btn-git");
+  btn_git.addEventListener("click", (event) => {
+    btn_git.classList.toggle("active");
+    console.log(event);
     });
   });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const btn_es6 = document.querySelector(".btn-es6");
+  btn_es6.addEventListener("click", (event) => {
+    btn_es6.classList.toggle("active");
+    console.log(event);
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+  const btn_css = document.querySelector(".btn-css");
+  btn_css.addEventListener("click", (event) => {
+    btn_css.classList.toggle("active");
+    console.log(event);
+  });
 });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+  const btn_html = document.querySelector(".btn-html");
+  btn_html.addEventListener("click", (event) => {
+    btn_html.classList.toggle("active");
+    console.log(event);
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+  const btn_cdt = document.querySelector(".btn-cdt");
+  btn_cdt.addEventListener("click", (event) => {
+    btn_cdt.classList.toggle("active");
+    console.log(event);
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+  const btn_heroku = document.querySelector(".btn-heroku");
+  btn_heroku.addEventListener("click", (event) => {
+    btn_heroku.classList.toggle("active");
+    console.log(event);
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+  const btn_openai = document.querySelector(".btn-openai");
+  btn_openai.addEventListener("click", (event) => {
+    btn_openai.classList.toggle("active");
+    console.log(event);
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const btn_copilot = document.querySelector(".btn-copilot");
+    btn_copilot.addEventListener("click", (event) => {
+      btn_copilot.classList.toggle("active");
+      console.log(event);
+      });
+    });
+
+  // Button click event lister for each frontend button //
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const btn_backend_ruby = document.querySelector(".btn-backend-ruby");
+    btn_backend_ruby.addEventListener("click", (event) => {
+      btn_backend_ruby.classList.toggle("active");
+      console.log(event);
+      });
+    });
